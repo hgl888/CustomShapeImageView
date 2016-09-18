ff102fec7881ee279247b10a157de19add24eb2c
hgl888 <hgl868@126.com>
2016-09-18 16:25:47 +0800

    添加函数

--- library/src/main/java/com/meg7/widget/CircleImageView.java
+++ library/src/main/java/com/meg7/widget/CircleImageView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -32,8 +35,46 @@ public class CircleImageView extends BaseImageView {
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.BLACK);
         canvas.drawOval(new RectF(0.0f, 0.0f, width, height), paint);
-
+//        canvas.drawBitmap(bitmap, 0, 0, paint);
         return bitmap;
+    }
+
+    public static Bitmap testBitmap(Bitmap bitmap)
+    {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.RGB_565);
+
+        Canvas canvas = new Canvas(output);
+
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        float[] array = {1,0,0,0,100,
+                0,1,0,0,100,
+                0,0,1,0,0,
+                0,0,0,1,0};
+        cm.set(array);
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        return output;
+    }
+
+    public static Bitmap test1Bitmap(Bitmap bitmap)
+    {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.RGB_565);
+
+        Canvas canvas = new Canvas(output);
+
+        Paint paint = new Paint();
+        Matrix cm = new Matrix();
+
+        float[] array = {1,0,50,
+                0,1,50,
+                0,0,1};
+        cm.setValues(array);
+        canvas.drawBitmap(bitmap, cm, paint);
+        return output;
     }
 
     @Override
